e5b22957a2edeb62d6f7e2f058e2082dd7893eb3
Valentin <valentin.riess@posteo.de>
2022-05-01 15:21:17 +0200

    Integrate hid feature

--- ovgumensabot/parser.py
+++ ovgumensabot/parser.py
@@ -1,13 +1,13 @@
-import logging
 from datetime import datetime
 from typing import List
 
 import pytz
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, ResultSet
 from maubot import Plugin
 
 from .meal import Meal
 from .menu import Menu
+
 TZ = pytz.timezone('Europe/Berlin')
 
 
@@ -43,19 +43,20 @@ def parse_table(menu_table) -> Menu:
                       meals=meals)
     return menu
 
+
 # parse hoersaal im dunkeln movie calendar
 # https://www.unifilm.de/studentenkinos/MD_HiD
-async def parse_movies(mensabot: Plugin, url: str) -> :
+async def parse_movies(mensabot: Plugin, url: str) -> list[tuple[datetime, str]]:
     async with mensabot.http.get(url) as resp:
         page = await resp.text()
-    mensabot.log.debug(page)
+    # mensabot.log.debug(page)
     soup = BeautifulSoup(page, "html.parser")
-    
+
     # locate calendar for current semester
     div_semester = soup.find("div", class_="kino-detail-spielplan spielplan-thisSemester")
 
     # locate all movies in current semester
-    div_movie = div_semester.find_all("div", class_="semester-film-row")
+    div_movie: List[BeautifulSoup] = div_semester.find_all("div", class_="semester-film-row")
 
     movies_dict = {}
 
@@ -63,9 +64,11 @@ async def parse_movies(mensabot: Plugin, url: str) -> :
     for movie in div_movie:
         date = movie.find("div", class_="film-row-text film-row-datum").text
         time = movie.find("div", class_="film-row-text film-row-uhrzeit").text
+        date_time_str = f"{date.strip().split(' ', 1)[1]}-{time.split(' ', 1)[0]}"
+        parsed_datetime: datetime = datetime.strptime(date_time_str, "%d.%m.%Y-%H:%M")
         # ignore space at the end of each title
-        title = movie.find("div", class_="film-row-text film-row-titel").strip()
-        
-        movies_dict[title] = {"date": date, "time": time}
-        
-    return movies_dict 
+        title = movie.find("div", class_="film-row-text film-row-titel").text.strip()
+
+        movies_dict[parsed_datetime] = title
+
+    return sorted(movies_dict.items())
